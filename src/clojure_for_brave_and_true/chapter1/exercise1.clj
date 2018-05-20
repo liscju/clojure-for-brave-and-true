61d800ddbd8e652c021aebc35d1a64a9bf3d1c9c
liscju <piotr.listkiewicz@gmail.com>
2018-05-20 19:19:26 +0200

    Add solution to chapter1/exercise1

---
+++ src/clojure_for_brave_and_true/chapter1/exercise1.clj
@@ -0,0 +1,11 @@
+(ns clojure-for-brave-and-true.chapter1.exercise1)
+
+(println (str "Hello" " " "World"))
+
+(println (vector 1 2 3 4 5))
+
+(println (list 1 2 3 4 5))
+
+(println (hash-map :key1 1 :key2 2))
+
+(println (hash-set :elem1 :elem2 :elem3))
