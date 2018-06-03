f83d6b93a3d40e724ad364e97d3a7b735083cfaf
liscju <piotr.listkiewicz@gmail.com>
2018-06-03 22:33:58 +0200

    Add solution to chapter5/exercise1

---
+++ src/clojure_for_brave_and_true/chapter5/exercise1.clj
@@ -0,0 +1,16 @@
+(ns clojure-for-brave-and-true.chapter5.exercise1)
+
+(def character
+  {:name "Smooches McCutes"
+   :attributes {:intelligence 10
+                :strength 4
+                :dexterity 5}})
+(def c-int (comp :intelligence :attributes))
+(def c-str (comp :strength :attributes))
+(def c-dex (comp :dexterity :attributes))
+
+(def attr
+  [a]
+  (comp a :attributes))
+
+
